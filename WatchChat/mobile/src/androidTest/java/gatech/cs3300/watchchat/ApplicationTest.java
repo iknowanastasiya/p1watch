961b409a6c444b015b71b912dbf9d49bb3778ed0
Kurt Bonatz <kbonatz3@gatech.edu>
2015-06-18 12:44:59 -0400

    Initial commit

---
+++ WatchChat/mobile/src/androidTest/java/gatech/cs3300/watchchat/ApplicationTest.java
@@ -0,0 +1,13 @@
+package gatech.cs3300.watchchat;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
