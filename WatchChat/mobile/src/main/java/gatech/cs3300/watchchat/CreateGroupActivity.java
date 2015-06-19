7201c691dc7eec7c9071bf6b81be9e5905374981
Tyler Bell <tyler@tylerwbell.com>
2015-06-19 14:59:03 -0400

    modal activities start

--- WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/CreateGroupActivity.java
+++ WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/CreateGroupActivity.java
@@ -1,23 +1,22 @@
 package gatech.cs3300.watchchat;
 
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class MainActivity extends ActionBarActivity {
+public class CreateGroupActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_group);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_create_group, menu);
         return true;
     }
 
