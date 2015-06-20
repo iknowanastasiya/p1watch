a7ede90ac6ea1b8df648c98b02ff13499fbf6953
Kurt Bonatz <kbonatz3@gatech.edu>
2015-06-20 17:43:26 -0400

    Logout and create group button added to Groups Activity.

--- WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/GroupsActivity.java
+++ WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/GroupsActivity.java
@@ -24,20 +24,24 @@ public class GroupsActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.LogoutButton) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.LogoutButton:
+                logout();
+                return true;
+            case R.id.CreateGroupButton:
+                createGroup();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void logout(){
         startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public void createGroup(){
+        startActivity(new Intent(this, CreateGroupActivity.class));
     }
 }
--- WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/MainActivity.java
+++ WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/MainActivity.java
@@ -1,11 +1,14 @@
 package gatech.cs3300.watchchat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,6 +19,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signIn = (Button) findViewById(R.id.SignInButton);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptSignIn();
+            }
+        });
+
+        Button register = (Button) findViewById(R.id.RegisterButton);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptRegister();
+            }
+        });
     }
 
     public void attemptSignIn(){
@@ -24,6 +43,7 @@ public class MainActivity extends Activity {
             Toast.makeText(getApplicationContext(), "Empty Username!", Toast.LENGTH_SHORT).show();
         } else{
             Toast.makeText(getApplicationContext(), "Signing In!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, GroupsActivity.class));
         }
     }
 
