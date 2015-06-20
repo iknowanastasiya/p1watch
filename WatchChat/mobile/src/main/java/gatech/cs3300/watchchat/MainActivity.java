5b77ac3c29b73905ec61a8d7700e974bb59294aa
Kurt Bonatz <kbonatz3@gatech.edu>
2015-06-20 16:15:51 -0400

    Sign in activity.

--- WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/MainActivity.java
+++ WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/MainActivity.java
@@ -3,8 +3,11 @@ package gatech.cs3300.watchchat;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -15,4 +18,21 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    public void attemptSignIn(){
+        EditText Username = (EditText) findViewById(R.id.Username);
+        if(TextUtils.isEmpty(Username.getText())){
+            Toast.makeText(getApplicationContext(), "Empty Username!", Toast.LENGTH_SHORT).show();
+        } else{
+            Toast.makeText(getApplicationContext(), "Signing In!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void attemptRegister(){
+        EditText Username = (EditText) findViewById(R.id.Username);
+        if(TextUtils.isEmpty(Username.getText())){
+            Toast.makeText(getApplicationContext(), "Empty Username!", Toast.LENGTH_SHORT).show();
+        } else{
+            Toast.makeText(getApplicationContext(), "Signing In!", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
