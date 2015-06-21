8d3f37f79e852eb1040b932087cf05db82ff3708
Tyler Bell <tyler@tylerwbell.com>
2015-06-21 00:03:28 -0400

    tweak layouts, menus

--- WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/GroupActivity.java
+++ WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/GroupActivity.java
@@ -1,5 +1,6 @@
 package gatech.cs3300.watchchat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -24,20 +25,15 @@ public class GroupActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_group, menu);
         return true;
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.GroupPreferencesButton) {
+            startActivity(new Intent(this, GroupPreferencesActivity.class));
             return true;
         }
 
