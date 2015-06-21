72999e3795ad373ac09272ae49b49b6f5c2696b9
Cotton, Anastasiya A <acotton6@gatech.edu>
2015-06-21 17:40:47 -0400

    added user login/register - not test

--- WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/APIController.java
+++ WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/APIController.java
@@ -1,6 +1,7 @@
 package gatech.cs3300.watchchat;
 
 import android.app.Activity;
+import android.content.Context;
 import android.preference.PreferenceActivity;
 import android.widget.Toast;
 
@@ -32,47 +33,74 @@ import java.util.List;
 
 public class APIController extends Activity {
 
-    public static final String API_ROOT = "http://cs3300.elasticbeanstalk.com";
+    public static final String API_ROOT = "http://cs3300.elasticbeanstalk.com/";
 
     //*To authenticate a user, send a GET request to /auth with the parameter name set to your username.
     // This request is responded by a unique int : userId. -1 is returned if the username does not exist*//
     public void authenticateUser(String username) {
-
+        System.out.println("Signing in");
         AsyncHttpClient client = new AsyncHttpClient();
         RequestParams params = new RequestParams();
-
         params.put("username", username);
-        client.get(API_ROOT + "/auth", params, new AsyncHttpResponseHandler() {
+        client.get(API_ROOT + "/users/auth", params, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 String response = new String(responseBody);
+                System.out.println("login page " +response);
                 if (response.equals("-1")) {
                     Toast.makeText(getApplicationContext(), "Requested username not found",
                             Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(getApplicationContext(), response,
-                            Toast.LENGTH_LONG).show();
+                    Toast.LENGTH_LONG).show();
                 }
             }
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody,
                                   Throwable error) {
-                try {
-                    Toast.makeText(getApplicationContext(), new String(responseBody),
-                            Toast.LENGTH_LONG).show();
+                System.out.println("API failure handler");
+                error.printStackTrace(System.out);
+                /*try {
+                     Toast.makeText(getApplicationContext(), new String(responseBody),
+                         Toast.LENGTH_LONG).show();
                 } catch (NullPointerException e) {
-                    Toast.makeText(getApplicationContext(), "Requested username not found",
-                            Toast.LENGTH_LONG).show();
-                }
+                     Toast.makeText(getApplicationContext(), "Requested username not found",
+                      Toast.LENGTH_LONG).show();
+                }*/
             }
         });
     }
 
     //*To request a list of all existing users, send a GET request to /users with no parameters.
     // This will return a JSON list of user's usernames as Strings *//
-    public ArrayList<String> getAllUsers() {
-        //TODO complete
-        return null;
+    public ArrayList<String>  getAllUsers() {
+        final ArrayList<String> userList = new ArrayList<String>();
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(API_ROOT + "users/all", new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    //ArrayList<String> userList = new ArrayList<>();
+                    String response = new String(responseBody);
+                    JSONArray jsonArray = new JSONArray(response);
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        String string = (String) jsonArray.getJSONObject(i).getString("username");
+                        userList.add(string);
+                    }
+                } catch (JSONException e) {
+                    System.out.println("JSONException in getAllUsers: " + e.getMessage());
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody,
+                                  Throwable error) {
+                Toast.makeText(getApplicationContext(), "Failure in getting contacts",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return userList; //userList;
     }
 
     //*To request information regarding an existing user, send a GET request to /users
@@ -92,7 +120,23 @@ public class APIController extends Activity {
     //*To create a user, send a POST request to /users passing the parameter username.
     // The username and userId will be returned.*//
     public void createUser(String username) {
-        //TODO complete
+        System.out.println("Register");
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("username", username);
+        client.post(API_ROOT + "users", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String response = new String(responseBody);
+                System.out.println(response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody,
+                                  Throwable error) {
+                error.printStackTrace(System.out);
+            }
+        });
     }
 
     //*To update information of an existing user, send a POST request
@@ -155,3 +199,4 @@ public class APIController extends Activity {
         //TODO complete
     }
 }
+
