a3d9d921f6cd9c785e1c393b38dcca0db127205b
Kurt Bonatz <kbonatz3@gatech.edu>
2015-06-22 13:53:13 -0400

    Added network capabilities for signing in and registering.

--- WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/MainActivity.java
+++ WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/MainActivity.java
@@ -4,16 +4,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+import org.json.JSONObject;
+
+import java.net.URL;
 
 
 public class MainActivity extends Activity {
 
-    APIController apiController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +42,6 @@ public class MainActivity extends Activity {
                 attemptRegister();
             }
         });
-
-      //  startActivity(new Intent(this, GroupActivity.class));
     }
 
     public void attemptSignIn(){
@@ -43,25 +49,92 @@ public class MainActivity extends Activity {
         if(TextUtils.isEmpty(Username.getText())){
             Toast.makeText(getApplicationContext(), "Empty Username!", Toast.LENGTH_SHORT).show();
         } else{
-            Toast.makeText(getApplicationContext(), "Signing In!", Toast.LENGTH_SHORT).show();
-            apiController = new APIController();
-            apiController.authenticateUser(Username.getText().toString());
-           //Toast.makeText(getApplicationContext(), "Signing In!", Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(this, GroupsActivity.class));
+            final Intent intent = new Intent(this, GroupsActivity.class);
+            final String username = Username.getText().toString();
+            AsyncHttpClient client = new AsyncHttpClient();
+            RequestParams params = new RequestParams();
+            params.put("username", username);
+            try{
+                URL url = new URL("http://cs3300.elasticbeanstalk.com" + "/users/auth");
+                client.post(url.toString(), params, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        try {
+                            JSONObject response = new JSONObject(new String(responseBody));
+                            if(response.getString("username").equals("-1")){
+                                Toast.makeText(getApplicationContext(), "Username does not exist!",
+                                        Toast.LENGTH_SHORT).show();
+                            } else{
+                                Log.d("UserId", response.getString("userId"));
+                                startGroupsActivity(response.getString("username"), response.getString("userId"));
+                            }
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "Parsing Error!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody,
+                                          Throwable error) {
+                        Log.e("Signin", error.getLocalizedMessage());
+                        error.printStackTrace(System.out);
+                        Toast.makeText(getApplicationContext(), "Network Error!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } catch(Exception e){
+                e.printStackTrace();
+            }
         }
     }
 
     public void attemptRegister(){
         EditText Username = (EditText) findViewById(R.id.Username);
-        System.out.println("userID "+ Username.getText().toString());
         if(TextUtils.isEmpty(Username.getText())){
             Toast.makeText(getApplicationContext(), "Empty Username!", Toast.LENGTH_SHORT).show();
         } else{
-            Toast.makeText(getApplicationContext(), "Registering!", Toast.LENGTH_SHORT).show();
-            apiController = new APIController();
-            apiController.createUser(Username.getText().toString());
-            //Toast.makeText(getApplicationContext(), "Registering!", Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(this, MainActivity.class));
+            final Intent intent = new Intent(this, GroupsActivity.class);
+            final String username = Username.getText().toString();
+            AsyncHttpClient client = new AsyncHttpClient();
+            RequestParams params = new RequestParams();
+            params.put("username", username);
+
+            try{
+                URL url = new URL(new String("http://cs3300.elasticbeanstalk.com" + "/users"));
+                client.post(url.toString(), params, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        try {
+                            JSONObject response = new JSONObject(new String(responseBody));
+
+                            Log.d("UserId", response.getString("userId"));
+                            startGroupsActivity(response.getString("username"), response.getString("userId"));
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "Parsing Error!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody,
+                                          Throwable error) {
+                        Log.e("Register", error.getLocalizedMessage());
+                        //error.printStackTrace(System.out);
+                        Toast.makeText(getApplicationContext(), "Network Error!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } catch(Exception e){
+                e.printStackTrace();
+            }
         }
+    }
+
+    public void startGroupsActivity(String username, String userId){
+        Intent intent = new Intent(this, GroupsActivity.class);
+        intent.putExtra("UserId", userId)
+                .putExtra("Username", username);
+        startActivity(intent);
     }
 }
