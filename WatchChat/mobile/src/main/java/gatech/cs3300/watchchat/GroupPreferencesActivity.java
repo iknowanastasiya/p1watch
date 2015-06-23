412a79127b6c608f4a41b1a13a94b27fd79429f6
unknown <acotton6@gatech.edu>
2015-06-23 03:06:19 -0400

    added new user to a group hardcoded userID and groupID

--- WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/GroupPreferencesActivity.java
+++ WatchChat/mobile/src/main/java/gatech/cs3300/watchchat/GroupPreferencesActivity.java
@@ -1,13 +1,27 @@
 package gatech.cs3300.watchchat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+import org.json.JSONObject;
+
+import java.net.URL;
 
 public class GroupPreferencesActivity extends ActionBarActivity implements AddGroupMemberDialog.AddGroupMemberDialogListener, ConfirmLeaveGroupDialog.ConfirmLeaveGroupDialogListener {
 
@@ -21,8 +35,8 @@ public class GroupPreferencesActivity extends ActionBarActivity implements AddGr
     private String[] mGroupMemberNames;
     private GroupMembersAdapter mGroupMemberAdapter;
 
-    private String[] testGroupNames() {
-        return new String[]{"Billy", "Susan", "Dirk"};
+   private String[] testGroupNames() {
+       return new String[]{"Billy", "Susan", "Dirk"};
     }
 
     @Override
@@ -86,10 +100,53 @@ public class GroupPreferencesActivity extends ActionBarActivity implements AddGr
 
     public void addGroupMemberWithName(String name) {
         mGroupActivityIndicator.setVisibility(View.VISIBLE);
-    }
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("groupId", 124387410);
+        params.put("userId", 3548);
+        try{
+            URL url = new URL("http://cs3300.elasticbeanstalk.com" + "/groups/add");
+            client.post(url.toString(), params, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    try {
+                        JSONObject response = new JSONObject(new String(responseBody));
+                        System.out.println(response);
+                        if(response.getString("groupName").equals("-1")){
+                            Toast.makeText(getApplicationContext(), "Error adding user to the group!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Log.d("groupName", response.getString("groupName"));
+                            startGroupActivity(response.getString("groupName"), response.getString("groupId"));
+                        }
+                    } catch (Exception e) {
+                        Toast.makeText(getApplicationContext(), "Parsing Error!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
 
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody,
+                                      Throwable error) {
+                    Log.e("Adding user", error.getLocalizedMessage());
+                    error.printStackTrace(System.out);
+                    Toast.makeText(getApplicationContext(), "Network Error!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
     public void leaveGroup() {
         mGroupActivityIndicator.setVisibility(View.VISIBLE);
     }
 
+    public void startGroupActivity(String username, String userId){
+        Intent intent = new Intent(this, GroupActivity.class);
+        intent.putExtra("UserId", userId)
+                .putExtra("Username", username);
+        startActivity(intent);
+    }
 }
