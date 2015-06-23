25dc96f6514912683f68507a27ac771b4e261509
Steven Mitchell <steven@lawn-143-215-120-152.lawn.gatech.edu>
2015-06-23 15:01:24 -0400

    Added starting template for Watch application

--- WatchChat/wear/src/main/java/gatech/cs3300/watchchat/MainActivity.java
+++ WatchChat/wear/src/main/java/gatech/cs3300/watchchat/MainActivity.java
@@ -3,7 +3,9 @@ package gatech.cs3300.watchchat;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +20,28 @@ public class MainActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+
+        TextView groupText = (TextView) findViewById(R.id.groupText);
+        groupText.setText("Test");
+
+        TextView messageText = (TextView) findViewById(R.id.messageText);
+        messageText.setText("Message");
+
+        Button replyButton = (Button) findViewById(R.id.replyButton);
+        replyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button groupButton = (Button) findViewById(R.id.groupButton);
+        replyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
