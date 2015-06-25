8cb83ff0486ce21167c10b69029680e1f920558b
Steven Mitchell <steven@lawn-143-215-120-152.lawn.gatech.edu>
2015-06-24 23:07:02 -0400

    Updated basic layout and functionality for Watch app

---
+++ WatchChat/wear/src/main/java/gatech/cs3300/watchchat/GroupListActivity.java
@@ -0,0 +1,90 @@
+package gatech.cs3300.watchchat;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.support.wearable.view.WearableListView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class GroupListActivity extends Activity implements WearableListView.ClickListener {
+
+    private WearableListView mListView;
+    private ArrayList<String> listItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_list);
+
+        listItems = new ArrayList();
+        listItems.add("Group 1");
+        listItems.add("Group 2");
+        listItems.add("Group 3");
+        listItems.add("Group 4");
+        listItems.add("Group 5");
+
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mListView = (WearableListView) stub.findViewById(R.id.listView);
+                mListView.setAdapter(new ListAdapter(GroupListActivity.this));
+                mListView.setClickListener(GroupListActivity.this);
+
+                Button cancelButton = (Button) findViewById(R.id.cancelButton);
+                cancelButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        setResult(Activity.RESULT_CANCELED);
+                        finish();
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onClick(WearableListView.ViewHolder viewHolder) {
+        int selectedIndex = viewHolder.getLayoutPosition();
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("group_name", listItems.get(selectedIndex));
+        setResult(Activity.RESULT_OK, resultIntent);
+        finish();
+    }
+
+    @Override
+    public void onTopEmptyRegionClick() {
+    }
+
+    private class ListAdapter extends WearableListView.Adapter {
+        private final LayoutInflater mInflater;
+
+        private ListAdapter(Context context) {
+            mInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public WearableListView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new WearableListView.ViewHolder(mInflater.inflate(R.layout.list_item, null));
+        }
+
+        @Override
+        public void onBindViewHolder(WearableListView.ViewHolder holder, int position) {
+            TextView view = (TextView) holder.itemView.findViewById(R.id.textView);
+            view.setText(listItems.get(position).toString());
+            holder.itemView.setTag(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return listItems.size();
+        }
+    }
+}
--- WatchChat/wear/src/main/java/gatech/cs3300/watchchat/MainActivity.java
+++ WatchChat/wear/src/main/java/gatech/cs3300/watchchat/MainActivity.java
@@ -1,6 +1,7 @@
 package gatech.cs3300.watchchat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.view.View;
@@ -9,17 +10,23 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+    static final int SELECT_REPLY_REQUEST = 0;
+    static final int SELECT_GROUP_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
+                // TODO: Get active group name
                 TextView groupText = (TextView) findViewById(R.id.groupText);
                 groupText.setText("Test");
 
+                // TODO: Get latest message from group
                 TextView messageText = (TextView) findViewById(R.id.messageText);
                 messageText.setText("Message");
 
@@ -27,18 +34,71 @@ public class MainActivity extends Activity {
                 replyButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
+                        Intent replyScreen = new Intent(MainActivity.this, ReplyActivity.class);
+                        startActivityForResult(replyScreen, SELECT_REPLY_REQUEST);
                     }
                 });
 
                 Button groupButton = (Button) findViewById(R.id.groupButton);
-                replyButton.setOnClickListener(new View.OnClickListener() {
+                groupButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
+                        Intent groupScreen = new Intent(MainActivity.this, GroupListActivity.class);
+                        startActivityForResult(groupScreen, SELECT_GROUP_REQUEST);
                     }
                 });
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SELECT_REPLY_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                // TODO: Call API to send message
+                //int replyNumber = data.getIntExtra("reply_number", -1);
+                String replyText = data.getStringExtra("reply_text");
+
+                TextView messageText = (TextView) findViewById(R.id.messageText);
+                messageText.setText(replyText);
+            }
+        }
+
+        if (requestCode == SELECT_GROUP_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String groupName = data.getStringExtra("group_name");
+
+                // Set group name to selected group
+                TextView groupText = (TextView) findViewById(R.id.groupText);
+                groupText.setText(groupName);
+
+                // Get the latest message from that group
+                TextView messageText = (TextView) findViewById(R.id.messageText);
+                String message;
+
+                // TODO: Call API to get latest message
+                switch (groupName) {
+                    case "Group 1": message = "That's awesome!";
+                        break;
+
+                    case "Group 2": message = "Hey, what's up?";
+                        break;
+
+                    case "Group 3": message = "Wanna grab lunch?";
+                        break;
+
+                    case "Group 4": message = "Have you finished the project yet?";
+                        break;
+
+                    case "Group 5": message = "iOS has the superior mobile development ecosystem.";
+                        break;
+
+                    default: message = "I hate Android Wear.";
+                        break;
+                }
+
+                messageText.setText(message);
+            }
+        }
     }
 }
